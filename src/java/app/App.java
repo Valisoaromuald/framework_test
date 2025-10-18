aa826b1810ccdefe44da4511be3d92bce07a19ba
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-10-18 10:33:03 +0300

    creation de la classe App et ClasseUrl

--- src/java/app/App.java
+++ src/java/app/App.java
@@ -1,7 +1,27 @@
 package app;
 
+import classes.ClasseUrl;
+import java.lang.reflect.Method;
+
+import annotation.UrlMapping;
+
 public class App {
     public static void main(String[] args) {
-        
+        try {
+            ClasseUrl classeUrl = new ClasseUrl();
+            Method[] methods =  classeUrl.getMethods();
+            for(Method method:methods){
+                if(method.isAnnotationPresent(UrlMapping.class)){
+                    UrlMapping mapping = method.getAnnotation(UrlMapping.class);
+                    Method m  = UrlMapping.class.getMethod("url");
+                    Object valeur = m.invoke(mapping);
+                    System.out.println("url: "+valeur);
+    
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("erreur : "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/java/classes/ClasseUrl.java
@@ -0,0 +1,22 @@
+package classes;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import annotation.UrlMapping;
+public class ClasseUrl {
+    @UrlMapping(url="/home")
+    public void home(){
+    }
+    @UrlMapping(url="/page-insertion")
+    public void pageInsertion(){
+
+    }
+    @UrlMapping(url="/delete")
+    public void supprimer(){
+    }
+    public Method[] getMethods(){
+        Class<ClasseUrl> classe = ClasseUrl.class;
+        Method[] methods = classe.getDeclaredMethods(); 
+        return methods;
+    }
+}
